271446f384ea0990971378e1995070a1db955f77
unknown <edyta_kopec@outlook.com>
2018-06-02 23:22:50 +0200

    UPDATE project

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8">
     <title>Historia lokalna-historia globalna</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 </head>
 
 <body>
 <header>
-    nagłówek- stała część strony
+    <div class="container text-center">
+        <div class="row header-img">
+            <img src="/images/heading-img.jpg" class="img-fluid">
+        </div>
+    </div>
+
 </header>
 <div class="container">
     @yield('content')
@@ -18,6 +23,9 @@
 
 @footer
 @endfooter
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 
 </html> 
