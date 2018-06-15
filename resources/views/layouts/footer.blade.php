4301d4d3d41ced6efb0e427ace4789afa029dd51
creativeEdition <edyta_kopec@outlook.com>
2018-06-15 23:58:33 +0200

    UPDATE footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,35 @@
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="col-12 col-lg-3">
-                logo
+<footer class="page-footer text-white font-small bg-dark pt-4 mt-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left mt-3 pb-3">
+
+            <div class="col-12 col-md-3 mx-auto mt-3 mb-4 logo-footer">
+                <img src="/images/logo.jpg">
             </div>
-            <div class="col-12 col-lg-3">
-                content stopki
+            <hr class="w-100 clearfix d-md-none">
+
+            <div class="col-12 col-md-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">O stronie</h6>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
-            <div class="col-12 col-lg-3">
-                content stopki
+            <hr class="w-100 clearfix d-md-none">
+
+            <div class="col-12 col-md-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Linki</h6>
+                <p><a href="#!">Twoje konto</a></p>
+                <p><a href="#!">Rejestracja</a></p>
+                <p><a href="#!">Pomoc</a></p>
             </div>
-            <div class="col-12 col-lg-3">
-                twitter insta i fb
+            <hr class="w-100 clearfix d-md-none">
+
+            <div class="col-12 col-md-3 mx-auto mt-3">
+                <h6 class="text-uppercase mb-4 font-weight-bold">Kontakt</h6>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-facebook mr-3"></i>info@mail.com</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-envelope mr-3"></i>info@mail.com</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-twitter mr-3"></i>info@mail.com</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-instagram mr-3"></i>info@mail.com</a></p>
+
             </div>
+            <hr class="w-100 clearfix d-md-none">
         </div>
         <div class="row text-center">
             <div class="col-12">
