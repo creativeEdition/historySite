d2f432180d585e55c482f2e44480992371140324
creativeEdition <edyta_kopec@outlook.com>
2018-06-17 15:00:08 +0200

    UPDATE footer fix bugs

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="page-footer text-white font-small bg-dark pt-4 mt-4">
     <div class="container text-center text-md-left">
-        <div class="row text-center text-md-left mt-3 pb-3">
+        <div class="row">
 
             <div class="col-12 col-md-3 mx-auto mt-3 mb-4 logo-footer">
                 <img src="/images/logo.jpg">
@@ -16,25 +16,39 @@
             <div class="col-12 col-md-3 mx-auto mt-3">
                 <h6 class="text-uppercase mb-4 font-weight-bold">Linki</h6>
                 <p><a href="#!">Twoje konto</a></p>
-                <p><a href="#!">Rejestracja</a></p>
+                <p><a href="#!">Regulamin</a></p>
                 <p><a href="#!">Pomoc</a></p>
             </div>
             <hr class="w-100 clearfix d-md-none">
 
             <div class="col-12 col-md-3 mx-auto mt-3">
                 <h6 class="text-uppercase mb-4 font-weight-bold">Kontakt</h6>
-                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-facebook mr-3"></i>info@mail.com</a></p>
-                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-envelope mr-3"></i>info@mail.com</a></p>
-                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-twitter mr-3"></i>info@mail.com</a></p>
-                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-instagram mr-3"></i>info@mail.com</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-envelope mr-3"></i>Napisz do nas!</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-facebook mr-3"></i>Nasz facebook</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-twitter mr-3"></i>Nasz twitter</a></p>
+                <p><a href="#" class="btn-floating btn-sm rgba-white-slight"><i class="fa fa-instagram mr-3"></i>Nasz instagram</a></p>
 
-            </div>
-            <hr class="w-100 clearfix d-md-none">
-        </div>
-        <div class="row text-center">
-            <div class="col-12">
-                <div id="copyright">© Copyright 2018 creativeEdition</div>
             </div>
         </div>
     </div>
+
+
+    <hr>
+    <!-- Call to action -->
+    <ul class="list-unstyled list-inline text-center py-2">
+        <li class="list-inline-item">
+            <h5 class="mb-1">Zarejestruj się!</h5>
+        </li>
+        <li class="list-inline-item">
+            <a href="#!" class="btn btn-danger btn-rounded">Rejestracja</a>
+        </li>
+    </ul>
+    <!-- Call to action -->
+    <hr>
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">
+        © Copyright 2018 creativeEdition
+    </div>
+    <!-- Copyright -->
+
 </footer>
